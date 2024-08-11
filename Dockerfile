04f70be112cac0e4f497b6634d5e8026700ef91e
danielhasid <117847806+danielhasid@users.noreply.github.com>
2024-08-11 07:54:23 +0300

    h

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ RUN apt-get update && apt-get install -y git
 WORKDIR /wizary
 COPY . /wizary
 RUN git clone https://github.com/danielhasid/wizary_prod.git
-WORKDIR /wizary/wizary_prod
+WORKDIR /wizary
 RUN pip install -r req.txt
+WORKDIR /wizary/wizary_prod
 CMD ["pytest"]
--- conftest.py
+++ conftest.py
@@ -4,13 +4,13 @@ import pytest
 
 @pytest.fixture(scope="session")
 def get_payload():
-    with open("./payload/json_data.json", "r") as file:
+    with open("../payload/json_data.json", "r") as file:
         data = json.load(file)
         return data
 
 @pytest.fixture(scope="session")
 def get_schema():
-    with open("./payload/expected_schem.json", "r") as file:
+    with open("../payload/expected_schem.json", "r") as file:
         data = json.load(file)
         return data
     
